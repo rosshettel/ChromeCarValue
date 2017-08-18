dd7af5567593d1475854522e8a754f37e447de63
Ross Hettel <ross@het.tel>
2017-08-18 08:35:44 -0500

    refactor

--- scraper-autotrader.js
+++ scraper-autotrader.js
@@ -1,23 +1,22 @@
 (function () {
-  var message = {
-    extension: 'ChromeCarValue',
-    'injected jquery version': jQuery.fn.jquery
-  };
-
-  var metadata = $('[data-birf-role="dataisland"]').data('birfExtra').page;
-  message.title = $('[data-qaid="cntnr-vehicle-title"]').text().toLowerCase();
-  message.title += " " + $('[data-qaid="cntnr-vehicle-trim"]').text().toLowerCase();
-  message.zip = metadata.location.address;
-  message.year = metadata.vehicle.car_year;
-  message.mileage = metadata.vehicle.odometer;
-  message.make = metadata.vehicle.make[0];
-  message.makeName = metadata.vehicle.makeName[0];
-  message.model = metadata.vehicle.model[0];
-  message.modelName = metadata.vehicle.modelName[0];
-  message.trim = metadata.vehicle.trim;
-  message.color = metadata.vehicle.color[0];
-  message.type = metadata.vehicle.Car_Type;
-  message.vin = metadata.vehicle.vin;
+  var metadata = $('[data-birf-role="dataisland"]').data('birfExtra').page,
+    title = $('[data-qaid="cntnr-vehicle-title"]').text() + " " + $('[data-qaid="cntnr-vehicle-trim"]').text(),
+    message = {
+      extension: 'ChromeCarValue',
+      'injected jquery version': jQuery.fn.jquery,
+      title: title.toLowerCase(),
+      zip: metadata.location.address,
+      year: metadata.vehicle.car_year,
+      mileage: metadata.vehicle.odometer,
+      make: metadata.vehicle.make[0],
+      makeName: metadata.vehicle.makeName[0],
+      model: metadata.vehicle.model[0],
+      modelName: metadata.vehicle.modelName[0],
+      trim: metadata.vehicle.trim,
+      color: metadata.vehicle.color[0],
+      type: metadata.vehicle.Car_Type,
+      vin: metadata.vehicle.vin
+    };
 
   chrome.runtime.sendMessage(message);
 })();
